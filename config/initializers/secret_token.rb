c4cc3ca0519223707863d4e33eceb024f6d9d547
Pratik Agarwal <pagarwal@mba2014.hbs.edu>
2013-11-20 12:49:46 -0500

    let's try

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Uberblog::Application.config.secret_key_base = '9c814ef902ce5dc9ac32d75c9a615f429299014db607b3f105a940e0d67f380e842b6769fb5a6eb7da757d929567ba124f87f64b6eed754c628b625eaccfa097'
